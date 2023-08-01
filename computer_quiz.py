70dae276159532d276c7f075d0d772547db60ded
Rachel <139805905+rachelevents@users.noreply.github.com>
2023-08-01 19:37:02 +0400

    Update computer_quiz.py

--- computer_quiz.py
+++ computer_quiz.py
@@ -1,6 +1,7 @@
 print("Welcome to my computer quiz!")
 playing = input("Do you want to play? ")
 if playing != "yes":
+    print('Okay, goodye!')
     quit()
 print("Okay! Let's play :)")
 answer = input("What does CPU stand for? ")
