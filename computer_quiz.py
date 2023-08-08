7e0040b4e4117ca83e75a2a77c582212588814af
Rachel <139805905+rachelevents@users.noreply.github.com>
2023-08-09 00:39:49 +0400

    Update computer_quiz.py

--- computer_quiz.py
+++ computer_quiz.py
@@ -1,20 +1,24 @@
 print("Welcome to my computer quiz!")
 playing = input("Do you want to play? ")
+
 if playing != "yes":
     print('Okay, goodye!')
     quit()
 print("Okay! Let's play :)")
 answer = input("What does CPU stand for? ")
+
 if answer == "central processing unit":
     print('Correct!')
 else:
     print("Incorrect!")
 answer = input("What does GPU stand for?")
+
 if answer == "graphics processing unit":
     print('Correct!')
 else:
     print("Incorrect!")
 answer = input("What does RAM stand for?")
+
 if answer == "random access memory":
     print('Correct!')
 else:
